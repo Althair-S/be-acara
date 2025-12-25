42c979d93f4a8f6966b430f579b693abd8c6200e
Althair-S <Althair.assassint@gmail.com>
2025-12-25 15:29:06 +0800

    feat : add api documentation for login and register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -57,6 +57,13 @@ export default {
 
     async login(req:Request, res:Response) {
         
+        /**
+        #swagger.requestBody = {
+            require : true,
+            schema: {$ref: "#/components/schemas/LoginRequest"}
+        }
+        */
+
         const { identifier, password} = req.body as unknown as TLogin;
         try {
             //ambil data user berdasarkan identifier -> email dan username
@@ -107,6 +114,11 @@ export default {
     },
 
     async me (req:IReqUser, res:Response) {
+        /**
+        #swagger.security = [{
+            "bearerAuth" : []
+        }]
+        */
         try {
             const user = req.user;
             const result = await UserModel.findById(user?.id);
---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info : {
+        version         : "v0.0.1",
+        tittle          : "Dokumentasi API ACARA",
+        description     : "Dokumentasi API ACARA",
+    },
+    servers : [
+        {
+            url         : "http://localhost:3000/api",
+            description : "Local Server",
+        },
+        {
+            url         : "https://back-end-acara.vercel.app/api",
+            description : "Deploy Server"
+        },
+    ],
+    components : {
+        securitySchemes : {
+            bearerAuth : {
+                type    : "http",
+                scheme  : "bearer"
+            },
+        },
+        schemas : {
+            LoginRequest : {
+                identifier : "elvians",
+                password   : "12341234",
+            },
+        },
+    }
+}
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen ({ openapi : "3.0.0"}) (outputFile, endpointsFiles, doc);
