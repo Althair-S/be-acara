6a94e8ab5645bfd55746ef18765e8567ce3156c5
Althair-S <Althair.assassint@gmail.com>
2026-01-21 21:07:40 +0800

    update dulu

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+import { IReqUser } from "../utils/interface";
+
+export default {
+    async create    () {},
+    async findAll   () {},
+    async findOne   () {},
+    async update    () {},
+    async remove    () {},
+
+};
